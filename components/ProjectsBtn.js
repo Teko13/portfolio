4424742e420a4dfae8112c8bbc04ef84c615f969
Teko13 <ffabrice999@gmail.com>
2025-09-13 21:09:03 +0200

    feat: Traduction du bouton Projects en français
    - Remplacé l'image rounded-text.png par un SVG personnalisé
    - Changé le texte de 'MY PROJECTS' vers 'MES PROJETS'
    - Conservé exactement le même style, taille et animation
    - Interface maintenant entièrement en français

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -14,13 +14,29 @@ const ProjectsBtn = () => {
         href={'/work'}
         className='relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group'
       >
-        <Image
-          src={'/rounded-text.png'}
-          width={141}
-          height={148}
-          alt=''
+        <svg
           className='animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]'
-        />
+          viewBox='0 0 141 148'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <defs>
+            <path
+              id='circle'
+              d='M 70.5, 74 m -50, 0 a 50,50 0 1,1 100,0 a 50,50 0 1,1 -100,0'
+            />
+          </defs>
+          <text
+            className='fill-white'
+            fontSize='16'
+            fontFamily='Arial, sans-serif'
+            fontWeight='600'
+            textAnchor='middle'
+          >
+            <textPath href='#circle' startOffset='0%'>
+              MES PROJETS • MES PROJETS • MES PROJETS • MES PROJETS • MES PROJETS • MES PROJETS •
+            </textPath>
+          </text>
+        </svg>
         <HiArrowRight className='absolute text-4xl group-hover:translate-x-2 transition-all duration-300' />
       </Link>
     </div>
